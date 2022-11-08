7239d1f0aec276494f1e31e151f2058eeeb30701
Nirav Prajapati <96686692+TheNiravPrajapati@users.noreply.github.com>
2022-11-08 14:04:00 +0530

    Update _dbconnect.php

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -3,7 +3,7 @@
 $servername ="localhost";
 $username = "root";
 $password = "";
-$database = "idisuss";
+$database = "idiscuss";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
